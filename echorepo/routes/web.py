05b8a20d2dd24638d48554113a1d22f13a08af57
osychenko <osychenko@gmail.com>
2026-07-22 22:45:00 +0000

    fix: incorrectly interpreted "% c" in sting replaced by a safe version

--- echorepo/routes/web.py
+++ echorepo/routes/web.py
@@ -408,7 +408,7 @@ def _js_base_labels() -> dict:
         "drawSelectionRectangle": _("Draw selection rectangle"),
         "notAvailable": _("Not available"),
         "zenodoDownload": _("Download dataset from Zenodo"),
-        "elementalConcentrationsHelp": _("Values reported in % can be converted to mg/kg by multiplying by 10000."),
+        "elementalConcentrationsHelp": _("Percentage values (%) can be converted to mg/kg by multiplying by 10000."),
         "unitConversionHelp": _("Unit conversion help"),
     }
 
